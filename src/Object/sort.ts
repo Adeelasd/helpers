cd57d9f9d9cabd748add7dd77429b62825067859
Adeel Ali <adeel.asd.aa@gmail.com>
2021-05-19 22:01:17 +0500

    fixes

--- src/Object/sort.ts
+++ src/Object/sort.ts
@@ -1,6 +1,6 @@
-const sort = (data:Array<[key:string]>,by:string)=>{
+const sort = (data:Array<{[key:string]:any}>,by:string)=>{
     let obj:any={}
-    data.map(item=> obj[by]=item)
+    data.map(item=> obj[item[by]]=item)
     return obj
 }
 export default sort
